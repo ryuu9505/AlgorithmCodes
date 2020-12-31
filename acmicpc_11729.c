0cf31a2b61242e1c5ba371042105a851745250dc
ryuu9505 <66634707+ryuu9505@users.noreply.github.com>
2021-01-01 03:37:41 +0900

    Create acmicpc_11729.c

---
+++ acmicpc_11729.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+void Hanoi(int n, int from, int by, int to)
+{
+	if (n == 1)
+	{
+		printf("%d %d\n", from, to);
+	}
+	else
+	{
+		Hanoi(n - 1, from, to, by);
+		printf("%d %d\n", from, to);
+		Hanoi(n - 1, by, from, to);
+	}
+}
+
+int main()
+{
+	int n, k;
+	scanf("%d", &n);
+	k = pow(2, n) - 1;
+	printf("%d\n", k);
+	Hanoi(n, 1, 2, 3);
+
+	return 0;
+}
